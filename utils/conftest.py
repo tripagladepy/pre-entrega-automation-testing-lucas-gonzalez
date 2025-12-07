b721758644578a0f430707b8d7f4eb24cd3b5ea3
tripagladepy <lucasjulian28@gmail.com>
2025-12-07 10:40:52 -0300

    Etapa 6: Integración CI/CD con GitHub Actions

--- utils/conftest.py
+++ utils/conftest.py
@@ -1,12 +1,73 @@
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# @pytest.fixture
+# def driver():
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--start-maximized")
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     yield driver
+#     driver.quit()
+
+
+
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# import os
+
+# @pytest.fixture
+# def driver():
+#     options = webdriver.ChromeOptions()
+
+#     # MODO NORMAL EN LOCAL
+#     options.add_argument("--start-maximized")
+
+#     # MODO HEADLESS EN GITHUB ACTIONS
+#     if os.getenv("HEADLESS") == "true":
+#         options.add_argument("--headless=new")
+#         options.add_argument("--no-sandbox")
+#         options.add_argument("--disable-dev-shm-usage")
+#         options.add_argument("--window-size=1920,1080")
+
+#     # Crear driver
+#     driver = webdriver.Chrome(
+#         service=Service(ChromeDriverManager().install()),
+#         options=options
+#     )
+
+#     yield driver
+#     driver.quit()
+
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+
+    # MODO NORMAL EN TU PC
     options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    # MODO HEADLESS EN GITHUB ACTIONS
+    if os.getenv("HEADLESS") == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
+    # Crear driver
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     yield driver
     driver.quit()
--- utils/logger.py
+++ utils/logger.py
@@ -5,16 +5,21 @@ from datetime import datetime
 # Crear carpeta logs
 os.makedirs("logs", exist_ok=True)
 
-# Formato
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 # Archivo: logs/ejecucion_YYYYMMDD.log
 filename = datetime.now().strftime("logs/ejecucion_%Y%m%d.log")
 
-logging.basicConfig(
-    filename=filename,
-    level=logging.INFO,
-    format=LOG_FORMAT
-)
+# Handler para archivo
+file_handler = logging.FileHandler(filename, encoding="utf-8")
+file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
+# Handler para consola
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+
+# Configurar logger
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
