3c25a99b876fb9b27e0be9178665df373750937f
tripagladepy <lucasjulian28@gmail.com>
2025-12-07 11:01:53 -0300

    Etapa 6: Fix Chrome Path y Headless driver setup

--- utils/conftest.py
+++ utils/conftest.py
@@ -43,6 +43,36 @@
 #     driver.quit()
 
 
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# import os
+
+# @pytest.fixture
+# def driver():
+#     options = webdriver.ChromeOptions()
+
+#     # MODO NORMAL EN TU PC
+#     options.add_argument("--start-maximized")
+
+#     # MODO HEADLESS EN GITHUB ACTIONS
+#     if os.getenv("HEADLESS") == "true":
+#         options.add_argument("--headless=new")
+#         options.add_argument("--no-sandbox")
+#         options.add_argument("--disable-dev-shm-usage")
+#         options.add_argument("--window-size=1920,1080")
+
+#     # Crear driver
+#     driver = webdriver.Chrome(
+#         service=Service(ChromeDriverManager().install()),
+#         options=options
+#     )
+
+#     yield driver
+#     driver.quit()
+
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -53,17 +83,20 @@ import os
 def driver():
     options = webdriver.ChromeOptions()
 
-    # MODO NORMAL EN TU PC
-    options.add_argument("--start-maximized")
-
-    # MODO HEADLESS EN GITHUB ACTIONS
+    # En Github Actions → headless y binary_location obligatorio
     if os.getenv("HEADLESS") == "true":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
+        options.binary_location = "/usr/bin/google-chrome-stable"
 
-    # Crear driver
+        # debug: print path del binario
+        options.binary_location = "/usr/bin/google-chrome-stable"
+        print("Using chrome binary:", options.binary_location)
+    else:
+        options.add_argument("--start-maximized")
+
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
